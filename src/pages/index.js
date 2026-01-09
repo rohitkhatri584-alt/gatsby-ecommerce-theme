6fc808b07f85136d2d117b5be76cc67b9424a817
rohitkhatri584-alt <rohitkhatri584@gmail.com>
2026-01-09 22:14:24 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={"Fabricvala - Your E‑commerce Growth Partner"}
+        subtitle={We help brands manage inventory, listings and shipping.}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
